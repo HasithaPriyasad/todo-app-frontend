4db5268e0eb8ccee3cb2b39566e1f0aecfd00b43
Hasitha Priyasad Wijerathna <hasithapriyasadmail@gmail.com>
2024-08-21 12:30:49 +0530

    taskStore fetchTask test added

--- tests/taskService.test.ts
+++ tests/taskService.test.ts
@@ -25,4 +25,38 @@ describe('taskService', () => {
     });
   });
 
+  describe('createTask', () => {
+    it('should call api.post with the correct endpoint and task data', async () => {
+      const taskData = { title: 'New Task', status: TaskStatus.NOTDONE };
+      const responseData = { id: '1', ...taskData };
+      const postMock = jest.spyOn(api, 'post').mockResolvedValueOnce({ data: responseData });
+
+      const result = await createTask(taskData);
+
+      expect(postMock).toHaveBeenCalledWith('/tasks', taskData);
+      expect(result).toEqual(responseData);
+    });
+  });
+
+  describe('updateTask', () => {
+    it('should call api.patch with the correct endpoint and task data', async () => {
+      const taskData = { id: '1', status: TaskStatus.DONE };
+      const responseData = { title: 'Example Task', ...taskData };
+      const patchMock = jest.spyOn(api, 'patch').mockResolvedValueOnce({ data: responseData });
+
+      const result = await updateTask(taskData);
+
+      expect(patchMock).toHaveBeenCalledWith(`/tasks/${taskData.id}`, taskData);
+      expect(result).toEqual(responseData);
+    });
+  });
+
+  describe('deleteTask', () => {
+    it('should call api.delete with the correct endpoint and task ID', async () => {
+      const taskId = '1';
+      const deleteMock = jest.spyOn(api, 'delete').mockResolvedValueOnce({});
+      await deleteTask(taskId);
+      expect(deleteMock).toHaveBeenCalledWith(`/tasks/${taskId}`);
+    });
+  });
 });
