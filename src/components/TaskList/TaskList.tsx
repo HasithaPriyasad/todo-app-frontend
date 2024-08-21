eb9d70507419028348e8e8fc4b0e6a8b42b5370d
Hasitha Priyasad Wijerathna <hasithapriyasadmail@gmail.com>
2024-08-21 13:24:51 +0530

    added the feature to display default todo when there is no task or search in operation

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -2,24 +2,28 @@ import React from 'react';
 import TaskItem from '../TaskItem/TaskItem';
 import { Task } from '../../types/Task';
 import { TaskStatus } from '../../enums/taskStatus';
+import { DEFAULT_TASK } from '../../constants/defaultTask';
 
 interface TaskListProps {
   tasks: Task[] | undefined;
   onStatusChange: (id: string, status: TaskStatus) => void;
   onDelete: (id: string) => void;
+  searchQuery: string;
 }
 
-const TaskList: React.FC<TaskListProps> = ({ tasks, onStatusChange, onDelete }) => {
+const TaskList: React.FC<TaskListProps> = ({ tasks, onStatusChange, onDelete, searchQuery }) => {
   return (
     <ul className="space-y-3">
-      {tasks?.map(task => (
+      {tasks !== undefined && tasks.length > 0 ? tasks?.map(task => (
         <TaskItem
           key={task.id}
           task={task}
           onStatusChange={onStatusChange}
           onDelete={onDelete}
         />
-      ))}
+      )):
+        searchQuery === '' && <TaskItem  key={1} task={DEFAULT_TASK} />
+      }
     </ul>
   );
 };
